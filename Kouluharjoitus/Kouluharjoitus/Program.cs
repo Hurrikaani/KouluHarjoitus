b3167fe83a70e8960908dee59b3418d6ddb101c3
Salminen Kim Petteri <kim.salminen@stud.pointcollege.fi>
2017-11-25 15:13:56 +0200

    Tiedostoon kirjoittaminen

---
+++ Kouluharjoitus/Kouluharjoitus/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kouluharjoitus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Lämpötila = 0;
+            Console.WriteLine("Anna uusi lämpötila");
+            String Syöte = Console.ReadLine();
+            Lämpötila = int.Parse(Syöte);
+
+            String tiedosto = @"C:\Koodailua\lämpötila.txt";
+
+            //Tiedostoon kirjoittaminen
+            File.WriteAllText(tiedosto, Lämpötila.ToString());
+            Console.WriteLine("Lämpötila on asetettu ja tallennettu");
+
+            Console.ReadLine();
+        }
+    }
+}
